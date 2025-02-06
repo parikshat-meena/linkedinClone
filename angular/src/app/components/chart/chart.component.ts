17b0bb97662c018a10a0e5101baa54efa5be8036
parikshat meena <parikshat.meena@cordiso.com>
2025-02-06 20:20:02 +0530

    chart added

--- angular/src/app/components/chart/chart.component.ts
+++ angular/src/app/components/chart/chart.component.ts
@@ -1,84 +1,37 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
-import { Chart, ChartConfiguration, ChartItem, registerables } from "chart.js";
+import { Component, ViewChild } from "@angular/core";
+import { ArcElement, Chart, Legend, PieController, Tooltip } from "chart.js";
+import { BaseChartDirective } from "ng2-charts";
+
+Chart.register(PieController, ArcElement, Tooltip, Legend);
+
 @Component({
   selector: "app-chart",
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: "./chart.component.html",
   styleUrl: "./chart.component.css",
 })
 export class ChartComponent {
-  @ViewChild("chartCanvas", { static: true })
-  chartCanvas!: any;
-  // @Input() chartConfig!: ChartConfiguration<"pie">;
-  // private chart!: Chart<"pie">;
-  // constructor() {
-  //   // Register necessary Chart.js components
-  //   Chart.register(...registerables);
-  // }
-  // ngOnInit(): void {
-  //   this.createChart();
-  // }
-  // private createChart(): void {
-  //   if (this.chartConfig) {
-  //     this.chart = new Chart<"pie">(
-  //       this.chartCanvas.nativeElement,
-  //       this.chartConfig
-  //     );
-  //   }
-  // }
-  // ngOnDestroy(): void {
-  //   if (this.chart) {
-  //     this.chart.destroy();
-  //   }
-  // }
-
-  data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
-
-  ele: any = document.getElementById("acquisitions");
-
-  async getChart() {
-    // new Chart(this.chartCanvas, {
-    //   type: "bar",
-    //   data: {
-    //     labels: this.data.map((row) => row.year),
-    //     datasets: [
-    //       {
-    //         label: "Acquisitions by year",
-    //         data: this.data.map((row) => row.count),
-    //       },
-    //     ],
-    //   },
-    // });
-    const ctx: any = document.getElementById("myChart");
-
-    new Chart(this.chartCanvas, {
-      type: "bar",
-      data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1,
-          },
-        ],
+  pieChartData = {
+    labels: ["Red", "Blue", "Yellow"],
+    datasets: [
+      {
+        data: [300, 500, 100],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
       },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
-        },
+    ],
+  };
+
+  pieChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
       },
-    });
-  }
+      tooltip: {
+        enabled: true,
+      },
+    },
+  };
 }
